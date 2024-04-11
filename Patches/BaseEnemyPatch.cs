b1471e467c60ef46d6d3131a3c70e157e7dc91da
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-10 21:30:38 -0600

    :art: Splitting patch

--- Patches/BaseEnemyPatch.cs
+++ Patches/BaseEnemyPatch.cs
@@ -27,16 +27,3 @@ internal static class BaseEnemyPatch
         }
     }
 }
-
-[HarmonyPatch(typeof(LongPressController), nameof(LongPressController.SetVisible))]
-internal static class LongPressEnemyPatch
-{
-    internal static void Postfix(LongPressController __instance, bool enable)
-    {
-        if (!SettingsManager.IsEnabled) return;
-        if (!enable) return;
-
-        var sac = __instance.m_Sac;
-        PressEnemyManager.AddCallBackPress(__instance.gameObject, sac.m_StartStar, sac.m_EndStar, sac.m_Mtrl);
-    }
-}
---
+++ Patches/LongPressEnemyPatch.cs
@@ -0,0 +1,18 @@
+﻿using FadeIn.Managers;
+using HarmonyLib;
+using Il2Cpp;
+
+namespace FadeIn.Patches;
+
+[HarmonyPatch(typeof(LongPressController), nameof(LongPressController.SetVisible))]
+internal static class LongPressEnemyPatch
+{
+    internal static void Postfix(LongPressController __instance, bool enable)
+    {
+        if (!SettingsManager.IsEnabled) return;
+        if (!enable) return;
+
+        var sac = __instance.m_Sac;
+        PressEnemyManager.AddCallBackPress(__instance.gameObject, sac.m_StartStar, sac.m_EndStar, sac.m_Mtrl);
+    }
+}
