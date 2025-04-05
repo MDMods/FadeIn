84ecdd9a2e504138a718db384e756bc09fc4d944
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2025-04-05 10:05:12 -0600

    🚧 Settings overhaul

--- Managers/SettingsManager.cs
+++ Managers/SettingsManager.cs
@@ -1,85 +1,208 @@
-﻿using MelonLoader;
+﻿using FadeIn.Properties;
+using MelonLoader;
+using MelonLoader.Preferences;
+using MelonLoader.Utils;
 
 namespace FadeIn.Managers;
 
-internal enum Difficulties
-{
-    Easy,
-    Medium,
-    Hard
-}
-
 internal static class SettingsManager
 {
-    private const string SettingsPath = "UserData/FadeIn.cfg";
+    internal static event FileSystemEventHandler WatcherEvent
+    {
+        add => Watcher.Changed += value;
+        remove => Watcher.Changed -= value;
+    }
 
     internal const float AlphaLowerLimit = 0.005f;
+    internal const string FileName = $"{MelonBuildInfo.ModName}.cfg";
+    internal const float MinimalDistanceR = 70f;
+    internal const float MinimalDistanceX = 5.8f;
+    internal const string Path = "UserData/" + FileName;
 
-    private static MelonPreferences_Entry<bool> _isEnabled;
+    #region properties
+    internal static float DisappearR => difficultySettings.DisappearR;
+    internal static float DisappearX => difficultySettings.DisappearX;
+    internal static Mode FadeMode => _fadeMode.Value;
+
     internal static bool IsEnabled
     {
         get => _isEnabled.Value;
         set => _isEnabled.Value = value;
     }
-    
-    private static MelonPreferences_Entry<string> _difficulty;
-    private static Difficulties _currentDifficulty;
-    internal static Difficulties Difficulty
+
+    internal static bool IsGameScene { get; private set; } = false;
+    internal static bool NeedReload { get; private set; } = false;
+    #endregion
+
+    #region fields
+    private static MelonPreferences_Category _category;
+    private static EnumEntry<Difficulty> _difficulty;
+    private static EnumEntry<Mode> _fadeMode;
+    private static MelonPreferences_Entry<bool> _isEnabled;
+    private static DifficultySettings difficultySettings;
+    private static readonly DifficultySettings Easy = new(-1.8f, 8f);
+    private static readonly DifficultySettings Hard = new(0f, 35f);
+    private static readonly DifficultySettings Medium = new(-0.9f, 20f);
+    private static readonly FileSystemWatcher Watcher = new(MelonEnvironment.UserDataDirectory);
+    #endregion
+
+    static SettingsManager() { }
+
+    #region methods
+
+    internal static void Init()
     {
-        get => _currentDifficulty;
+        WatcherConfig();
 
-        set
-        {
-            _currentDifficulty = value;
-            switch (_currentDifficulty)
-            {
-                case Difficulties.Easy:
-                    _difficulty.Value = "Easy";
-                    DisappearPositionX = -1.8f;
-                    DisappearPositionR = 8f;
-                    break;
+        _category = MelonPreferences.CreateCategory(MelonBuildInfo.ModName);
+        _category.SetFilePath(Path, false, false);
 
-                case Difficulties.Hard:
-                    _difficulty.Value = "Hard";
-                    DisappearPositionX = 0f;
-                    DisappearPositionR = 35f;
-                    break;
+        _isEnabled = _category.CreateEntry(
+            "Enabled",
+            true,
+            description: "Enable or disable the mod!"
+        );
 
-                default:
-                    _difficulty.Value = "Medium";
-                    DisappearPositionX = -0.9f;
-                    DisappearPositionR = 20f;
-                    break;
-            }
-        }
-    }
+        _difficulty = new EnumEntry<Difficulty>(_category, "Difficulty", Difficulty.Medium);
+        _fadeMode = new EnumEntry<Mode>(_category, "FadeMode", Mode.FadeOut);
 
-    internal static float DisappearPositionX { get; private set; }
-    internal static float DisappearPositionR { get; private set; }
-    internal static float MinimalDistanceX { get; private set; }
-    internal static float MinimalDistanceR { get; private set; }
-
-    private static void InitValues()
-    {
-        Difficulty = _difficulty.Value switch
-        {
-            "Easy" => Difficulties.Easy,
-            "Hard" => Difficulties.Hard,
-            _ => Difficulties.Medium
-        };
-
-        MinimalDistanceX = 5.8f;
-        MinimalDistanceR = 70f;
+        Load();
     }
 
     internal static void Load()
     {
-        var settings = MelonPreferences.CreateCategory("FadeIn");
-        settings.SetFilePath(SettingsPath, true, false);
+        _category.LoadFromFile(true);
 
-        _isEnabled = settings.CreateEntry(nameof(IsEnabled), false);
-        _difficulty = settings.CreateEntry(nameof(Difficulty), "Medium", description: "Options:\nEasy\nMedium\nHard");
+        difficultySettings = _difficulty.Value switch
+        {
+            Difficulty.Easy => Easy,
+            Difficulty.Medium => Medium,
+            Difficulty.Hard => Hard,
+            _ => Medium,
+        };
+    }
 
-        InitValues();
+    internal static void Reload(bool sceneChanged = false)
+    {
+        if (IsGameScene)
+        {
+            NeedReload = true;
+            return;
+        }
+
+        if (!sceneChanged)
+        {
+            Load();
+            NeedReload = false;
+            return;
+        }
+
+        if (!NeedReload)
+        {
+            return;
+        }
+
+        Load();
+        NeedReload = false;
+        return;
+    }
+
+    internal static void SceneChanged(string sceneName)
+    {
+        IsGameScene = sceneName?.Equals("GameMain") ?? false;
+
+        Reload(true);
+    }
+
+    internal static void WatcherStart()
+    {
+        // It is highly recommended to only enable events on melon late start...
+        Watcher.EnableRaisingEvents = true;
+    }
+
+    private static void WatcherConfig()
+    {
+        Watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size;
+
+        Watcher.Filter = FileName;
+
+        WatcherEvent += (_, _) =>
+        {
+            Reload(false);
+        };
+    }
+
+    #endregion
+
+    internal class EnumEntry<T>
+        where T : struct, Enum, IConvertible
+    {
+        internal T Value => _entry.Value;
+
+        private readonly T _defaultEnumValue;
+        private readonly MelonPreferences_Entry<T> _entry;
+
+        internal EnumEntry(MelonPreferences_Category category, string name, T defaultValue)
+        {
+            _defaultEnumValue = defaultValue;
+            _entry = category.CreateEntry(
+                name,
+                _defaultEnumValue,
+                validator: new EnumEntryValidator(defaultValue)
+            );
+
+            // Todo: add description (show options)
+        }
+
+        internal void Subscribe(LemonAction<T, T> action)
+        {
+            _entry.OnEntryValueChanged.UnsubscribeAll();
+            _entry.OnEntryValueChanged.Subscribe(action);
+        }
+
+        private sealed class EnumEntryValidator : ValueValidator
+        {
+            public readonly T DefaultValue;
+
+            public EnumEntryValidator(T defaultValue)
+            {
+                DefaultValue = defaultValue;
+            }
+
+            public override object EnsureValid(object value)
+            {
+                return Enum.TryParse(value.ToString().Trim(), true, out T result)
+                    ? result
+                    : DefaultValue;
+            }
+
+            public override bool IsValid(object value) => true;
+        }
+    }
+
+    internal enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    internal enum Mode
+    {
+        FadeIn,
+        FadeOut,
+        Random
+    }
+
+    private readonly struct DifficultySettings
+    {
+        public float DisappearR { get; init; }
+        public float DisappearX { get; init; }
+
+        public DifficultySettings(float disappearX, float disappearR)
+        {
+            DisappearX = disappearX;
+            DisappearR = disappearR;
+        }
     }
 }
